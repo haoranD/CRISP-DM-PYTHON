d8c0399c37e1fef0cb7191254885c4f7beda38d8
Haoran Duan <haoranduan28@gmail.com>
2018-11-18 21:58:07 +0000

    New py file

--- src/data/EDA.py
+++ src/data/EDA.py
@@ -36,51 +36,6 @@ def files(data_file_name):
 
     return times_list, times_count, length_each
 
-def Data_Type(data_file_name):
-    num_times = 1
-    times_list = []
-    times_count = []
-    tmp_data = []
-    length_each = []
-    nameCount = []
-    just_count = 0
-
-    for i in range(len(data_file_name) + 1):
-
-        if i >= len(data_file_name):
-            times_list.append(tmp_data)
-            nameCount.append('Term_' + str(num_times))
-        else:
-            if int(data_file_name[i][15]) != num_times:
-                length_each.append(i - just_count)
-                nameCount.append('Term_' + str(num_times))
-                num_times += 1
-                times_list.append(tmp_data)
-                tmp_data = []
-                just_count = i
-            # print(num_times)
-            tmp_data.append(data_file_name[i][17:-4])
-            times_count.append('Term_' + str(num_times))
-    data_file = pd.DataFrame(times_list, index=nameCount)
-
-    return data_file
-
-
-def Each_data(Main_Data_Path, data_file_name):
-    tmp_dataframe_list = []
-    tmp_csv_list = []
-
-    for j in data_file_name:
-        Each_file = Main_Data_Path + j
-
-        with open(Each_file) as file:
-            tmp_dataframe_list.append((str(j), pd.read_csv(file)))
-            tmp_csv_list.append((str(j), csv.reader(file)))
-    Data_frame = dict(tmp_dataframe_list)
-    Data_csv = dict(tmp_csv_list)
-
-    return Data_frame,Data_csv
-
 def Get_Location():
 
     return
