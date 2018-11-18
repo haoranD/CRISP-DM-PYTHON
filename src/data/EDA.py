09af04cc7772e0c7a492f217aa8ff16af8b1b8f2
Haoran Duan <haoranduan28@gmail.com>
2018-11-18 21:51:59 +0000

    New Function

--- src/data/EDA.py
+++ src/data/EDA.py
@@ -1,10 +1,20 @@
-import numpy as np
-import pandas as pd
 import sklearn
 import csv
 from os import listdir
+import numpy as np
+from geopy.geocoders import Nominatim
+import plotly
+plotly.tools.set_credentials_file(username='haoran88', api_key='Dvsr2pAhI49jMnWPa6Vo')
+import pandas as pd
+from os import listdir
+from src.data import EDA
+import csv
+from IPython.display import display
+from time import time
+import os
+import sys
 
-def check_files(data_file_name):
+def files(data_file_name):
     num_times = 1
     times_list = []
     times_count = []
@@ -25,6 +35,35 @@ def check_files(data_file_name):
         times_count.append('Time_' + str(num_times))
 
     return times_list, times_count, length_each
+
+def Data_Type(data_file_name):
+    num_times = 1
+    times_list = []
+    times_count = []
+    tmp_data = []
+    length_each = []
+    nameCount = []
+    just_count = 0
+
+    for i in range(len(data_file_name) + 1):
+
+        if i >= len(data_file_name):
+            times_list.append(tmp_data)
+            nameCount.append('Term_' + str(num_times))
+        else:
+            if int(data_file_name[i][15]) != num_times:
+                length_each.append(i - just_count)
+                nameCount.append('Term_' + str(num_times))
+                num_times += 1
+                times_list.append(tmp_data)
+                tmp_data = []
+                just_count = i
+            # print(num_times)
+            tmp_data.append(data_file_name[i][17:-4])
+            times_count.append('Term_' + str(num_times))
+    data_file = pd.DataFrame(times_list, index=nameCount)
+
+    return data_file
 
 
 def Each_data(Main_Data_Path, data_file_name):
